f66bfdca5adc9c6cc643ede420d9b8d94a6fb8c5
Pantorn <longpantorn@gmail.com>
2024-08-02 18:50:12 +0700

    prototype

--- src/index.php
+++ src/index.php
@@ -32,23 +32,33 @@ try {
 <body>
 
 
-  <div class="text-[46px] text-bold justify-center items-center flex flex-col mt-3 ">
+  <div class="text-[46px] text-bold mt-3 w-[55%]">
     <!-- <div class="text-[46px] text-bold grid grid-flow-col auto-cols-auto"> -->
-    TITLE
     <?php
     foreach ($result as $row) {
       $name = $row['name'];
       $talent = $row['talent'];
       $description = $row['description'];
       $image = $row["uri"];
-      echo ("
-      <div>$name</div>
-      <div>$talent</div>
-      <div>$description</div>
-      <div> <img src='$image' class='w-40 h-40' alt='singer image'></div>
-      ");
-    };
+
     ?>
+      <div class='[&>div]:flex ml-5 [&>div]:justify-between [&>div]:mb-5'>
+        <div class="">
+          TITLE
+          <div><?php echo $name ?></div>
+        </div>
+        <div>
+          <div>Talent</div>
+          <div><?php echo $talent ?></div>
+        </div>
+        <div>
+          <div>Description</div>
+          <div class="text-[1rem] w-[50%]"><?php echo $description ?></div>
+        </div>
+      </div>
+      <img src='<?php echo $image ?>' class='m-5 absolute top-0 right-0 object-cover object-center w-[40%] h-[80vh] mb-[50%]' alt='singer image'>
+
+    <?php }; ?>
   </div>
 
 </body>
