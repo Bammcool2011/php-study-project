41a7b9463a7658db0a557cb24c3f6f9ab3dc371b
Pumin Wangprasit <savee2011@hotmail.com>
2024-08-03 08:36:27 +0700

    updated html

--- src/pumin.php
+++ src/pumin.php
@@ -11,12 +11,12 @@
 <body class="bg-gray-100 text-gray-800">
   <div class="container mx-auto px-4 py-8">
     <header class="flex justify-center mb-8">
-      <h1 class="text-5xl font-bold">Porter Robinson</h1>
+      <h1 class="text-5xl font-bold">PORTER ROBINSON</h1>
     </header>
 
-    <section class="bg-white shadow-md rounded-lg p-6">
+    <section class="bg-white shadow-md rounded-lg p-6 mb-8">
       <h2 class="text-2xl font-semibold mb-4 text-center">Who is Porter Robinson?</h2>
-      <p class="text-center">
+      <p class="text-justify">
         Porter Robinson is an American electronic music producer, singer, and DJ known for his emotive and
         genre-blending sound. Born on July 15, 1992, in Atlanta, Georgia, he gained prominence with his 2011 single
         "Language" and debut album <em>Worlds</em> (2014), which showcased a unique blend of electronic, synth-pop, and
@@ -27,9 +27,14 @@
     </section>
 
     <section class="bg-white shadow-md rounded-lg p-6">
+      <h2 class="text-2xl font-semibold mb-4 text-center">His works<h2>
+          <div class="flex flex-wrap">
+            <div class="w-1/3">01</div>
+            <div class="w-1/3">02</div>
+            <div class="w-1/3">03</div>
+          </div>
 
     </section>
-
 
   </div>
 </body>
